ea66bb6f794f637b743683d781551dbf1d935a8b
Saul Amster <saulamster@gmail.com>
2017-12-02 18:05:36 -0500

    Intial commit

---
+++ server.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# Python middleman server for bystander effect
+# Ludum Dare 40
+# Copyright Benjamin Welsh and Saul Amster
+
+import SimpleHTTPServer
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import SocketServer
+import os.path
+
+PORT=30002
+
+class Server(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def _set_headers_json(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+
+    def do_GET(self):
+        self._set_headers()
+        localpath="serve"+self.path
+        if localpath == "serve/":
+            localpath="serve/root.html"
+        print localpath
+        if os.path.isfile(localpath):
+            print "file exists"
+            with open(localpath) as f:
+                self.wfile.write(f.read())
+        else:
+            self.wfile.write("<html><body><h1>404</h1></body></html>")
+
+
+    def do_HEAD(self):
+        self._set_headers()
+
+    def do_POST(self):
+        # Doesn't do anything with posted data
+        self._set_headers()
+#        print self.data
+        self.wfile.write('{"name":"bob"}')
+
+
+httpd = SocketServer.TCPServer(("", PORT), Server)
+
+print "serving at port", PORT
+try:
+    httpd.serve_forever()
+except KeyboardInterrupt as e:
+    httpd.server_close()
